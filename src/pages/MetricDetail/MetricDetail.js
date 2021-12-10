d7a2dfab20d81ccac828e83313bdc3444e04870d
ivannovobranets <flashniv@gmail.com>
2021-12-10 16:17:37 +0200

    Add trimm double values to 3 chars

--- src/pages/MetricDetail/MetricDetail.js
+++ src/pages/MetricDetail/MetricDetail.js
@@ -5,6 +5,25 @@ import Graphic from "../../components/Graphic/Graphic";
 import Table from "../../components/Table/Table";
 import classes from "./MetricDetail.module.css";
 
+function formatDate(date) {
+    var month = '' + (date.getMonth() + 1),
+        day = '' + date.getDate(),
+        year = date.getFullYear(),
+        minutes = ''+date.getMinutes(),
+        seconds = ''+date.getSeconds();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    if (minutes.length < 2) 
+        minutes = '0' + minutes;
+    if (seconds.length < 2) 
+        seconds = '0' + seconds;
+
+    return [year, month, day].join('-')+" "+date.getHours()+":"+minutes+":"+seconds;
+}
+
 function MetricDetail(){
     let div=useRef(null)
     let { path }=useParams();
@@ -38,6 +57,10 @@ function MetricDetail(){
                     newName+=key+":"+value+" ";
                 }
                 lim.name=newName
+                lim.min=lim.min.toFixed(3)
+                lim.max=lim.max.toFixed(3)
+                lim.last=lim.last.toFixed(3)
+                lim.avg=lim.avg.toFixed(3)
                 locLimits.push(lim)
             })
             setLimits(locLimits)
@@ -45,7 +68,25 @@ function MetricDetail(){
         )    
         const responseEvents=APIServer.getContent('/apiv1/gui/metrics/events?path='+path+'&beginPeriod='+begin+'&endPeriod='+end)
         responseEvents.then(value =>{
-            setEvents(value.data)
+            let locEvents=[]
+            value.data.map(event =>{
+                var newObj={}
+                Object.keys(event).map(key =>{
+                    if(key.localeCompare("time")==0){
+                        newObj.time=formatDate(new Date(event.time))
+                    }else{
+                        let value=event[key].toFixed(3);
+                        let newKey=""
+                        key=JSON.parse(key)
+                        for (const [name, value] of Object.entries(key)) {
+                            newKey+=name+":"+value+" ";
+                        }
+                        newObj[newKey]=value
+                    }
+                })
+                locEvents.push(newObj)
+            })
+            setEvents(locEvents)
         }
         )    
     },[end,begin])

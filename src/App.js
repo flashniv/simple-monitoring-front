5e9ab1fe69b97879eb7a8eef6ece3e94285b7627
flashniv <flashniv@gmail.com>
2022-08-07 12:24:07 +0300

    v2. init

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import React, {useState} from "react";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import NavBar from "./components/NavBar/NavBar";
-import "./styles/App.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Metrics from "./pages/Metrics/Metrics";
-import MetricDetail from "./pages/MetricDetail/MetricDetail";
-import History from "./pages/History/History";
-import AlertFilters from "./pages/AlertFilters/AlertFilters";
-import Logout from "./components/Logout/Logout";
-import APIServer from "./API/APIServer";
-import SignIn from "./pages/SignIn/SignIn";
-import IndexPage from "./pages/Index/IndexPage";
-import {createTheme, ThemeProvider} from "@mui/material";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(APIServer.isLoggedIn)
-    const theme=createTheme({
-        palette: {
-            primary: {
-                main: '#2368f3',
-            },
-            secondary: {
-                main: '#66385a',
-            },
-        },
-    })
-    return (
-        <BrowserRouter>
-            <ThemeProvider theme={theme}>
-                <NavBar loggedIn={loggedIn}/>
-                <Routes>
-                    <Route path="/" element={<IndexPage/>}/>
-                    <Route path="/signin" element={<SignIn setLoggedIn={setLoggedIn}/>}/>
-                    {loggedIn
-                        ? <>
-                            <Route path="/dashboard" element={<Dashboard/>}/>
-                            <Route path="/metrics" element={<Metrics/>}/>
-                            <Route path="/metricDetail">
-                                <Route path=":path" element={<MetricDetail/>}/>
-                            </Route>
-                            <Route path="/history" element={<History/>}/>
-                            <Route path="/alertFilters" element={<AlertFilters/>}/>
-                            <Route path="/logout" element={<Logout setLoggedIn={setLoggedIn}/>}/>
-                        </>
-                        : <></>
-                    }
-                </Routes>
-            </ThemeProvider>
-        </BrowserRouter>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
